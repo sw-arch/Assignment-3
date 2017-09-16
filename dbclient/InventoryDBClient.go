c691c12ccbcec052aa6fffa5d7949c7a0e408f6d
Jacob Mason <jacob@jacobmason.net>
2017-09-15 19:22:52 -0500

    Add db connection info for Inventory
    Signed-off-by: Jacob Mason <jacob@jacobmason.net>

--- dbclient/InventoryDBClient.go
+++ dbclient/InventoryDBClient.go
@@ -1,11 +1,51 @@
 package dbclient
 
 import (
-    "github.com/satori/go.uuid"
     "Assignment-3/dao"
+    "database/sql"
+
+    _ "github.com/mattn/go-sqlite3"
+    "github.com/satori/go.uuid"
 )
 
 type InventoryDBClient struct {
+    database *sql.DB
+}
+
+var inventoryDBInstance *InventoryDBClient
+
+func GetPurchaseDBClient() *InventoryDBClient {
+    if inventoryDBInstance == nil {
+        inventoryDBInstance = &InventoryDBClient{initializeInventoryDB()}
+        inventoryDBInstance.createInventoryTable()
+    }
+    return inventoryDBInstance
+}
+
+func initializeInventoryDB() *sql.DB {
+    db, err := sql.Open("sqlite3", "inventory.db")
+    checkErr(err)
+    if db == nil {
+        panic("DB is nil!")
+    }
+
+    return db
+}
+
+func (client InventoryDBClient) createInventoryTable() {
+    checkStatement := "SELECT name FROM sqlite_master WHERE type='table' AND name='inventory';"
+    result, err := client.database.Exec(checkStatement)
+    checkErr(err)
+    if result == nil {
+        createStatement := `Create Table inventory (
+            inventory_id Text primary key,
+            name Text,
+            description Text,
+            price Real
+            );`
+        _, err := client.database.Exec(createStatement)
+        checkErr(err)
+    }
 }
 
 func (client InventoryDBClient) GetItemByID(id uuid.UUID) dao.InventoryItem {
@@ -26,4 +66,10 @@ func (client InventoryDBClient) Release(item dao.InventoryItem, quantity uint64)
 
 func (client InventoryDBClient) Remove(item dao.InventoryItem, quantity uint64) bool {
     return false
+}
+
+func checkErr(err error) {
+    if err != nil {
+        panic(err)
+    }
 }
