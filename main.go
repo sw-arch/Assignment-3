841d08a90d3865056441d74dce030753ee57e98f
nathantfrank <nathan.t.frank@gmail.com>
2017-09-17 15:30:25 -0500

    added cli and boilerplate login cmd

--- main.go
+++ main.go
@@ -1,13 +1,42 @@
 package main
 
 import (
-	"fmt"
 	// "Assignment-3/dbclient"
+	"github.com/abiosoft/ishell"
 )
 
+func checkCredintials(username string, password string) bool {
+	return true
+}
+
 func main() {
-	for {
-		fmt.Println("1. Create User")
-		fmt.Println("2. Get User By Username")
-	}
+	shell := ishell.New()
+
+	shell.Println("Welcome to the Store")
+
+	// Login cmd
+	shell.AddCmd(&ishell.Cmd{
+		Name: "login",
+		Help: "Authentification for login",
+		Func: func(c *ishell.Context) {
+			c.ShowPrompt(false)
+			defer c.ShowPrompt(true)
+
+			c.Print("Username: ")
+			username := c.ReadLine()
+
+			c.Print("Password: ")
+			password := c.ReadPassword()
+
+			// do something with username and password
+			if checkCredintials(username, password) {
+				c.Println("Authentication Successful.")
+				// add commands to shell
+			} else {
+				c.Println("Authentication Failed.")
+			}
+		},
+	})
+
+	shell.Run()
 }
