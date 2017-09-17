c7cb7d774447d85297c1ac32a2be4eb1fed4449e
nathantfrank <nathan.t.frank@gmail.com>
2017-09-16 20:17:06 -0500

    added sql querries and rearranged inventory struct variables

--- dao/InventoryItem.go
+++ dao/InventoryItem.go
@@ -3,9 +3,9 @@ package dao
 import "github.com/satori/go.uuid"
 
 type InventoryItem struct {
-    QuantityAvailable int
-    Name              string
-    Description       string
-    Price             float64
-    Id                *uuid.UUID
+	Id                *uuid.UUID
+	Name              string
+	Description       string
+	Price             float64
+	QuantityAvailable int
 }
--- dbclient/InventoryDBClient.go
+++ dbclient/InventoryDBClient.go
@@ -1,49 +1,97 @@
 package dbclient
 
 import (
-    "Assignment-3/dao"
-    "database/sql"
+	"Assignment-3/dao"
+	"database/sql"
 
-    _ "github.com/mattn/go-sqlite3"
-    "github.com/satori/go.uuid"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/satori/go.uuid"
 )
 
 type InventoryDBClient struct {
-    db *sql.DB
+	db *sql.DB
 }
 
 var inventoryDBInstance *InventoryDBClient
 
 func GetInventoryDBClient() *InventoryDBClient {
-    if inventoryDBInstance == nil {
-        inventoryDBInstance = &InventoryDBClient{initializeDB("inventory.db")}
-        createTable(inventoryDBInstance.db, "inventory",
-            `inventory_id Integer primary key,
+	if inventoryDBInstance == nil {
+		inventoryDBInstance = &InventoryDBClient{initializeDB("inventory.db")}
+		createTable(inventoryDBInstance.db, "inventory",
+			`inventory_id Text primary key,
             name Text,
             description Text,
             price Real,
             quantity_on_hand Integer,
             quantity_reserved Integer`)
-    }
-    return inventoryDBInstance
+	}
+	return inventoryDBInstance
 }
 
 func (client InventoryDBClient) GetItemByID(id uuid.UUID) dao.InventoryItem {
-    return dao.InventoryItem{}
+	statement, prepErr := client.db.Prepare("SELECT * FROM inventory WHERE inventory_id=?;")
+	checkErr(prepErr)
+
+	row := statement.QueryRow(id)
+	var uUID uuid.UUID
+	var name string
+	var description string
+	var price float64
+	var quantity_on_hand int
+	var quantity_reserved int
+	err := row.Scan(&uUID, &name, &description, &price, &quantity_on_hand, &quantity_reserved)
+	checkErr(err)
+	return dao.InventoryItem{&id, name, description, price, quantity_on_hand}
 }
 
-func (client InventoryDBClient) SetItemQuantity(quantity uint64) bool {
-    return false
+func (client InventoryDBClient) SetItemQuantity(itemId uuid.UUID, quantity uint64) bool {
+	statement, prepErr := client.db.Prepare("UPDATE inventory SET quantity_on_hand=? where id=?;")
+	checkErr(prepErr)
+
+	res, execErr := statement.Exec(quantity, itemId.String())
+	checkErr(execErr)
+
+	rowCount, err := res.RowsAffected()
+	checkErr(err)
+
+	return rowCount != 0
 }
 
 func (client InventoryDBClient) Reserve(item dao.InventoryItem, quantity uint64) bool {
-    return false
+	statement, prepErr := client.db.Prepare("UPDATE inventory SET quantity_reserved=quantity_reserved - ? where id=?;")
+	checkErr(prepErr)
+
+	res, execErr := statement.Exec(quantity, item.Id.String())
+	checkErr(execErr)
+
+	rowCount, err := res.RowsAffected()
+	checkErr(err)
+
+	return rowCount != 0
 }
 
 func (client InventoryDBClient) Release(item dao.InventoryItem, quantity uint64) bool {
-    return false
+	statement, prepErr := client.db.Prepare("UPDATE inventory SET quantity_reserved=quantity_reserved + ? where id=?;")
+	checkErr(prepErr)
+
+	res, execErr := statement.Exec(quantity, item.Id.String())
+	checkErr(execErr)
+
+	rowCount, err := res.RowsAffected()
+	checkErr(err)
+
+	return rowCount != 0
 }
 
 func (client InventoryDBClient) Remove(item dao.InventoryItem, quantity uint64) bool {
-    return false
+	statement, prepErr := client.db.Prepare("UPDATE inventory SET quantity_on_hand=quantity_on_hand - ? where id=?;")
+	checkErr(prepErr)
+
+	res, execErr := statement.Exec(quantity, item.Id.String())
+	checkErr(execErr)
+
+	rowCount, err := res.RowsAffected()
+	checkErr(err)
+
+	return rowCount != 0
 }
