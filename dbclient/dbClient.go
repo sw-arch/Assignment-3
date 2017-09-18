a0f86d06514fb3bdc26f367d6018369505cd41fa
Jacob Mason <jacob@jacobmason.net>
2017-09-18 15:09:24 -0500

    Fix createTable select query
    Signed-off-by: Jacob Mason <jacob@jacobmason.net>

--- dbclient/dbClient.go
+++ dbclient/dbClient.go
@@ -17,13 +17,15 @@ func initializeDB(dbName string) *sql.DB {
 }
 
 func createTable(db *sql.DB, tableName string, schema string) {
-	selectStatement, prepSelectErr := db.Prepare("SELECT name FROM sqlite_master WHERE type='table' AND name='?';")
+	selectStatement, prepSelectErr := db.Prepare("SELECT name FROM sqlite_master WHERE type='table' AND name=?")
 	checkErr(prepSelectErr)
+	defer selectStatement.Close()
 
-	result, selectErr := selectStatement.Exec(tableName)
-	checkErr(selectErr)
-	if result == nil {
-		createStatement, prepCreateErr := db.Prepare("Create Table ? (?);")
+	var name string
+	selectErr := selectStatement.QueryRow(tableName).Scan(&name)
+
+	if selectErr == sql.ErrNoRows {
+		createStatement, prepCreateErr := db.Prepare("CREATE TABLE ? (?)")
 		checkErr(prepCreateErr)
 		_, createErr := createStatement.Exec(tableName, schema)
 		checkErr(createErr)
