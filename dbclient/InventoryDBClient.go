a30dd9aa2941e9b6273163f593cdd49231e5c7c0
Jacob Mason <jacob@jacobmason.net>
2017-09-20 00:29:52 -0500

    Fix quantity available math
    Signed-off-by: Jacob Mason <jacob@jacobmason.net>

--- dbclient/InventoryDBClient.go
+++ dbclient/InventoryDBClient.go
@@ -171,7 +171,7 @@ func makeInventoryItemFromRow(row *sql.Row) dao.InventoryItem {
 	var quantityReserved int64
 	rowErr := row.Scan(&item.Id, &item.Name, &item.Description, &item.Category, &item.AttributeOne, &item.AttributeTwo, &item.Price, &quantityOnHand, &quantityReserved)
 	checkErr(rowErr)
-	item.QuantityAvailable = quantityOnHand
+	item.QuantityAvailable = quantityOnHand - quantityReserved
 	return item
 }
 
@@ -183,7 +183,7 @@ func makeInventoryItemsFromRows(rows *sql.Rows) []dao.InventoryItem {
 		var quantityReserved int64
 		rowErr := rows.Scan(&item.Id, &item.Name, &item.Description, &item.Category, &item.AttributeOne, &item.AttributeTwo, &item.Price, &quantityOnHand, &quantityReserved)
 		checkErr(rowErr)
-		item.QuantityAvailable = quantityOnHand
+		item.QuantityAvailable = quantityOnHand - quantityReserved
 		items = append(items, item)
 	}
 
